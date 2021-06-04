12ab510d7af1ef1357f66f673aabf4b3a703d639
MiguelAngelLopezH <36934468+MiguelAngelLopezH@users.noreply.github.com>
2021-06-03 22:02:42 -0500

    Delete SWAutobus directory

--- SWAutobus/src/main/java/com/example/SWAutobus/ViajeCRUD.java
+++
@@ -1,7 +0,0 @@
-package com.example.SWAutobus;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ViajeCRUD extends CrudRepository<Viajes,Integer> {
-    Viajes findByViaje(String viaje);
-}
